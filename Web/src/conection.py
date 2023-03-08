291c80e3b54a7c8e09b9376f2ae57bfd851eaf28
JesusJiga <jesusjiga.developer@gmail.com>
2023-03-08 19:00:23 +0100

    probando cache

--- Web/src/conection.py
+++ Web/src/conection.py
@@ -1,5 +1,7 @@
 import pymongo
+import streamlit as st
 
+@st.cache_resource
 def get_conection(key):
     client = pymongo.MongoClient(f"mongodb+srv://JiGa:{key}@viad.b6yfn8g.mongodb.net/?retryWrites=true&w=majority")
 
